b3972dec50ade871d7bdb1c1ca88a5064549acd3
Dreamtell <2827812603@qq.com>
2026-07-28 22:31:31 +0800

    test

--- Minecraft存档迁移工具.py
+++ Minecraft存档迁移工具.py
@@ -314,10 +314,7 @@ class SplashScreen:
             # 状态文字（分段渲染 emoji + 中文）
             import re
             status = self.status_texts[self.text_index]
-            emoji_pattern = re.compile(
-                "[\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002700-\U000027BF\U00002600-\U000026FF]",
-                re.UNICODE
-            )
+            emoji_pattern = re.compile("[\U0001F300-\U0001F5FF\U0001F600-\U0001F64F\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002700-\U000027BF\U00002600-\U000026FF]",re.UNICODE)
             parts = emoji_pattern.split(status)
             emojis = emoji_pattern.findall(status)
             x_offset = bar_x
